fda90fdb63a37c514063ced955a46117f660b6cb
Benny G <bganea@onelogic.fr>
2018-06-07 14:16:17 +0300

    Initial

---
+++ tests/Demo1/DemoTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Benny
+ * Date: 07/06/2018
+ * Time: 14:01
+ */
+namespace Test\Demo1;
+
+class DemoTest{
+	public function testDemo(){
+		$this->assertTrue(true);
+	}
+}
