8427fc5a417cc3a60a49ec633f99dd6d99c1e731
Benny G <bganea@onelogic.fr>
2018-06-13 17:41:30 +0300

    Phase 3

---
+++ tests/Demo4/DoublesTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Benny
+ * Date: 13/06/2018
+ * Time: 17:35
+ */
+
+namespace Test\Demo4;
+
+use PHPUnit\Framework\TestCase as BaseTest;
+
+class DoublesTest extends BaseTest{
+	/**
+	 * 115 - Următoarele sunt cele mai comune tipuri de dubluri de test
+	 * - Dummy - acesta este un container care nu este apelat sau utilizat,
+	 * este folosit numai atunci când trebuie să passezi required arguments
+	 * - Fake - acesta imita obiectul si functionalitatea lui, dar este scris
+	 * si folosit numai pentru teste
+	 * - Stub
+	 * - Spy
+	 * - Mock
+	 */
+}
