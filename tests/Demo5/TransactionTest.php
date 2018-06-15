caa5c56fe141e88cff893d7c282b39b702e7cea9
Benny G <bganea@onelogic.fr>
2018-06-15 16:05:49 +0300

    Phase 5

--- tests/Demo5/TransactionTest.php
+++ tests/Demo5/TransactionTest.php
@@ -12,6 +12,7 @@ use PHPUnit\Framework\TestCase as BaseTest;
 use Code\Demo5\FakeLogger;
 use Code\Demo5\Transaction;
 use SimpleXMLElement;
+use Code\Demo5\HttpClient;
 
 class TransactionTest extends BaseTest{
 	private $data;
@@ -30,7 +31,10 @@ class TransactionTest extends BaseTest{
 
 	public function testPrepareXMLRequest() {
 		$logger = new FakeLogger();
-		$client = $this->getMock('HttpClient', array(), array('http://localhost'));
+		$client=$this->getMockBuilder(HttpClient::class)
+			->setConstructorArgs(['http://localhost'])
+			->getMock();
+		//$client = $this->getMock('HttpClient', array(), array('http://localhost'));
 		$transaction = new Transaction($logger, $client, $this->data);
 		$request = new SimpleXMLElement(
 			$transaction->prepareXMLRequest());
@@ -42,17 +46,33 @@ class TransactionTest extends BaseTest{
 
 	public function testPrepareXMLRequestFail(){
 		$logger = new FakeLogger();
-		$client = $this->getMock('HttpClient', array(), array('http://localhost'));
-		$dataMissingUserId = $this->data;
-		unset($dataMissingUserId['userId']);
-		$transaction = new Transaction($logger, $client, $dataMissingUserId);
+		$client=$this->getMockBuilder(HttpClient::class)
+			->setConstructorArgs(['http://localhost'])
+			->getMock();
+		//$client = $this->getMock('HttpClient', array(), array('http://localhost'));
+		//$dataMissingUserId = $this->data;
+		//unset($dataMissingUserId['userId']);
+		//$transaction = new Transaction($logger, $client, $dataMissingUserId);
+		$transaction = new Transaction($logger, $client, $this->data);
 		$xmlRequest = $transaction->prepareXMLRequest();
+
+		$request = new SimpleXMLElement($xmlRequest);
+
+		$item = $request->items->item[0];
+		$this->assertEquals("1", $request['userId']);
+		$this->assertEquals("1", $item['id']);
+		$this->assertEquals("99", $item['quantity']);
+		return $xmlRequest;
 	}
 
 	public function testSendRequest()
 	{
 		$logger = new FakeLogger();
-		$client = $this->getMock('HttpClient', array('send'), array('http://localhost'));
+		$client=$this->getMockBuilder(HttpClient::class)
+			->setConstructorArgs(['http://localhost'])
+			->setMethods(['send'])
+			->getMock();
+		//$client = $this->getMock('HttpClient', array('send'), array('http://localhost'));
 		$client
 			->expects($this->any())
 			->method('send')
